e4dddc20da776994564a8aeb24f80cd9e53e45ba
Allan Ritchie <allan.ritchie@gmail.com>
2022-07-06 13:10:52 -0400

    Updates

--- DigitalScoreboard/MauiProgram.cs
+++ DigitalScoreboard/MauiProgram.cs
@@ -1,6 +1,7 @@
 ﻿using CommunityToolkit.Maui;
 using DigitalScoreboard.Infrastructure;
 using Microsoft.Extensions.Configuration;
+using Prism.DryIoc;
 
 namespace DigitalScoreboard;
 
@@ -12,33 +13,21 @@ public static class MauiProgram
 		var builder = MauiApp
 			.CreateBuilder()
 			.UseMauiCommunityToolkit()
-            .UseShiny()
-			.UsePrismApp<App>(prism => prism
-                .RegisterTypes(registry =>
-                {
-                    registry.RegisterForNavigation<NavigationPage>();
-                    registry.RegisterForNavigation<MainPage, MainViewModel>();
-                    registry.RegisterForNavigation<SettingsPage, SettingsViewModel>();
-                    registry.RegisterForNavigation<RefereePage, RefereeViewModel>();
-                    registry.RegisterForNavigation<ScoreboardPage, ScoreboardViewModel>();
-                })
-                .OnAppStart(
-                    async navigator => await navigator.Navigate("NavigationPage/MainPage")
-                )
+            .UseShinyFramework<App>(
+                new DryIocContainerExtension(),
+                prism => prism
+                    .RegisterTypes(registry =>
+                    {
+                        registry.RegisterForNavigation<NavigationPage>();
+                        registry.RegisterForNavigation<MainPage, MainViewModel>();
+                        registry.RegisterForNavigation<SettingsPage, SettingsViewModel>();
+                        registry.RegisterForNavigation<RefereePage, RefereeViewModel>();
+                        registry.RegisterForNavigation<ScoreboardPage, ScoreboardViewModel>();
+                    })
+                    .OnAppStart(
+                        async navigator => await navigator.Navigate("NavigationPage/MainPage")
+                    )
             )
-            //         .UseShinyFramework<App>(prism => prism
-            //             .RegisterTypes(registry =>
-            //             {
-            //                 registry.RegisterForNavigation<NavigationPage>();
-            //                 registry.RegisterForNavigation<MainPage, MainViewModel>();
-            //                 registry.RegisterForNavigation<SettingsPage, SettingsViewModel>();
-            //                 registry.RegisterForNavigation<RefereePage, RefereeViewModel>();
-            //                 registry.RegisterForNavigation<ScoreboardPage, ScoreboardViewModel>();
-            //             })
-            //             .OnAppStart(
-            //                 async navigator => await navigator.Navigate("NavigationPage/MainPage")
-            //             )
-            //)
             .ConfigureFonts(fonts =>
 			{
 				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
@@ -47,19 +36,26 @@ public static class MauiProgram
 				fonts.AddFont("electron.ttf", "electron");
 			});
 
-		builder.Configuration.AddJsonPlatformBundle(optional: false);
-		builder.Services.AddSingleton(builder.Configuration.GetSection("Bluetooth").Get<BluetoothConfig>());
-        //builder.Services.AddSingleton(new BluetoothConfig
-        //{
-        //	ServiceUuid = "144340bf-3566-425e-98ff-e57aab8c6360",
-        //	CharacteristicUuid = "144340bf-3566-425e-98ff-e57aab8c6361"
-        //      });
-        builder.Services.AddSingleton(AppInfo.Current);
-
+		//builder.Configuration.AddJsonPlatformBundle(optional: false);
+		//builder.Services.AddSingleton(builder.Configuration.GetSection("Bluetooth").Get<BluetoothConfig>());
+        builder.Services.AddSingleton(new BluetoothConfig
+        {
+            ServiceUuid = "144340bf-3566-425e-98ff-e57aab8c6360",
+            CharacteristicUuid = "144340bf-3566-425e-98ff-e57aab8c6361"
+        });
         builder.Services.AddSingleton(DeviceDisplay.Current);
         builder.Services.AddShinyService<AppSettings>();
 
-        builder.Services.UseGlobalCommandExceptionHandler();
+        builder.Services.AddGlobalCommandExceptionHandler(options =>
+        {
+#if DEBUG
+            options.AlertType = ErrorAlertType.FullError; // this will show the full error in a dialog during debug
+            options.LogError = false;
+#else
+            options.AlertType = ErrorAlertType.NoLocalize;
+            options.LogError = true;
+#endif
+        });
 
         builder.Services.AddBluetoothLE();
 		builder.Services.AddBluetoothLeHosting();
--- readme.md
+++ readme.md
@@ -3,8 +3,11 @@
 
 ## TODO
 
-* Better error handling on online logging
+* Online Error Log - AppCenter, Firebase, Sentry, etc
 * Yards-to-go for 1st down
+* Timeouts Remaining
+* Game Clock needs "total minutes:seconds"
+* Better design
 
 ## Ideas
 
@@ -13,3 +16,98 @@
     * Play Clock
     * Game Clock
     * Score
+
+
+<ContentPage.Resources>
+        <ResourceDictionary>
+            <Style TargetType="VerticalStackLayout">
+                <Setter Property="VerticalOptions" Value="FillAndExpand" />
+                <Setter Property="HorizontalOptions" Value="FillAndExpand" />
+            </Style>
+
+            <Style x:Key="Main" TargetType="Border">
+                <Setter Property="Stroke" Value="White" />
+                <Setter Property="StrokeThickness" Value="10" />
+            </Style>
+            <!--<Style TargetType="Frame">
+                <Setter Property="BackgroundColor" Value="Black" />
+                <Setter Property="BorderColor" Value="White" />
+                <Setter Property="CornerRadius" Value="10" />
+            </Style>-->
+            <Style x:Key="Title" TargetType="Label">
+                <Setter Property="FontSize" Value="70" />
+                <Setter Property="TextColor" Value="White" />
+                <Setter Property="FontFamily" Value="{Binding Font}" />
+                <Setter Property="HorizontalTextAlignment" Value="Center" />
+            </Style>
+
+            <Style x:Key="Info" TargetType="Label">
+                <Setter Property="FontSize" Value="88" />
+                <Setter Property="TextColor" Value="Yellow" />
+                <Setter Property="FontFamily" Value="{Binding Font}" />
+                <Setter Property="HorizontalTextAlignment" Value="Center" />
+            </Style>
+        </ResourceDictionary>
+    </ContentPage.Resources>
+
+    <ContentPage.Content>
+        <Border Margin="0" Style="{StaticResource Main}">
+
+            <Grid RowDefinitions="1*, 1*" ColumnDefinitions="1*, 2*, 1*">
+
+                <VerticalStackLayout Grid.Row="0" Grid.Column="0">
+                    <!--
+                    <TapGestureRecognizer Command="{Binding SetHomeScore}" />
+                    <Label Text="{Binding HomeTeamName}" />
+                    <Label Text="{Binding HomeTeamScore}" />
+                    -->
+                </VerticalStackLayout>
+
+                <VerticalStackLayout Grid.Row="0" Grid.Column="1">
+                    <Border Style="{StaticResource Main}" >
+                        <!--
+                        <TapGestureRecognizer Command="{Binding TogglePeriodClock}" />
+
+                        <Label Text="{Binding PeriodClock, StringFormat='{0:c}'}" />
+                        -->
+                    </Border>
+
+
+                    <!--
+                    <TapGestureRecognizer Command="{Binding IncrementPeriod}" />
+                    <Label Text="{Binding Period}" />
+                    -->
+                </VerticalStackLayout>
+
+                <VerticalStackLayout Grid.Row="0" Grid.Column="2">
+                    <!--
+                    <TapGestureRecognizer Command="{Binding SetAwayScore}" />
+ 
+                    <Label Text="{Binding AwayTeamName}" />
+
+                    <Label Text="{Binding AwayTeamScore}" />
+                    -->
+                </VerticalStackLayout>
+
+                <VerticalStackLayout Grid.Row="1" Grid.Column="0">
+                    <!--
+                    <TapGestureRecognizer Command="{Binding IncrementDown}" />
+                    <Label Text="{Binding Down}" />
+                    -->
+
+                </VerticalStackLayout>
+
+                <VerticalStackLayout Grid.Row="1" Grid.Column="1">
+                    <!--yards to go-->
+                    <!--ball on the X-->
+                </VerticalStackLayout>
+
+                <VerticalStackLayout Grid.Row="1" Grid.Column="2">
+                    <!--
+                    <TapGestureRecognizer Command="{Binding TogglePlayClock}" />
+                    <Label Text="{Binding PlayClock}" />
+                    -->
+                </VerticalStackLayout>
+            </Grid>
+        </Border>
+    </ContentPage.Content>
